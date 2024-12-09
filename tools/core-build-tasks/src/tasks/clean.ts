fbb378a45dd15e29713af281e1bfbc22f1387751
Pieter Maes <maescool@gmail.com>
2024-12-09 01:32:56 +0100

    Fix: Make cleanTask synchronous This is to prevent it removing files when you 'copyFiles' to the same path directly after. This happens when using 'updateWorldTask' 'cleanCollateralTask' also is synchronous

--- tools/core-build-tasks/src/tasks/clean.ts
+++ tools/core-build-tasks/src/tasks/clean.ts
@@ -9,10 +9,12 @@ export const DEFAULT_CLEAN_DIRECTORIES = ['temp', 'lib', 'dist'];
 export function cleanTask(dirs: string[]) {
     return () => {
         for (const dir of dirs) {
-            console.log(`Cleaning ${dir}`);
-            rimraf(path.resolve(process.cwd(), dir), () => {
-                // no-op on unable to clean
-            });
+            try {
+                console.log(`Cleaning ${dir}`);
+                rimraf.sync(path.resolve(process.cwd(), dir));
+            } catch (_: unknown) {
+                // File or directory did not exist, so we no-op
+            }
         }
     };
 }

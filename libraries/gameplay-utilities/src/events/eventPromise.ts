b0490c9a6d97abf204ab9739b2cd4f1ea2bd4690
Andrew Griffin <agriffin@microsoft.com>
2025-09-16 16:24:05 -0700

    Working promise

--- libraries/gameplay-utilities/src/events/eventPromise.ts
+++ libraries/gameplay-utilities/src/events/eventPromise.ts
@@ -6,26 +6,61 @@ import { world, system } from '@minecraft/server';
 /**
  * A promise wrapper utility which returns a new promise that will resolve when the next
  * event is raised.
- * 
+ *
  * @public
  */
-export interface EventPromise {
+export interface EventPromise<T> extends Promise<T | undefined> {
     /**
      * Cancels the promise and unsubscribes from the event signal. Cancellation is done
      * by fulfilling with undefined.
-     * 
+     *
      * @public
      */
     cancel(): void;
+
+    /**
+     * Promise-like interface then.
+     *
+     * @param onfulfilled Called if the promise fulfills
+     * @param onrejected Called if the promise rejects
+     */
+    then<TFulfill = T | undefined, TReject = never>(
+        onfulfilled?: ((value: T | undefined) => TFulfill | PromiseLike<TFulfill>) | null,
+        onrejected?: ((reason: unknown) => TReject | PromiseLike<TReject>) | null
+    ): Promise<TFulfill | TReject>;
+
+    /**
+     * Promise-like interface catch.
+     *
+     * @param onrejected Called if the promise rejects
+     */
+    catch<TReject = never>(
+        onrejected?: ((reason: unknown) => TReject | PromiseLike<TReject>) | null
+    ): Promise<T | undefined | TReject>;
+
+    /**
+     * Promise-like interface finally.
+     *
+     * @param onfinally Called when the promise resolves
+     */
+    finally(onfinally?: (() => void) | null): Promise<T | undefined>;
 }
+
+type MinecraftAfterEventSignals =
+    | (typeof world.afterEvents)[keyof typeof world.afterEvents]
+    | (typeof system.afterEvents)[keyof typeof system.afterEvents];
+type FirstArg<T> = T extends (arg: infer U) => void ? U : never;
 
 /**
  * Helper to create a new EventPromise from an after event signal.
  *
  * @public
  */
-export function nextEvent<U>(signal: (typeof world.afterEvents[keyof typeof world.afterEvents]) | (typeof system.afterEvents[keyof typeof system.afterEvents]), filter?: U) : EventPromise {
-    return new EventPromiseImpl(signal, filter);
+export function nextEvent<U>(
+    signal: MinecraftAfterEventSignals,
+    filter?: U
+): EventPromise<FirstArg<FirstArg<typeof signal.subscribe>>> {
+    return new EventPromiseImpl<FirstArg<FirstArg<typeof signal.subscribe>>, U>(signal, filter);
 }
 
 /**
@@ -34,39 +69,65 @@ export function nextEvent<U>(signal: (typeof world.afterEvents[keyof typeof worl
  *
  * @private
  */
-class EventPromiseImpl<T, U = undefined> extends Promise<T | undefined> {
+class EventPromiseImpl<T, U> implements Promise<T | undefined> {
+    [Symbol.toStringTag] = 'Promise';
+    private promise: Promise<T | undefined>;
     private onCancel?: () => void;
 
-    constructor(signal: typeof world.afterEvents[keyof typeof world.afterEvents], filter?: U) {
-        let cancelFn: (() => void) | undefined = undefined;
-        super((resolve, _) => {
-            let sub: (event: T) => void;
-            if (filter === undefined) {
-                sub = signal.subscribe(event => {
-                    this.onCancel = undefined;
-                    signal.unsubscribe(sub);
-                    resolve(event);
-                });
-            } else {
-                sub = signal.subscribe(event => {
-                    this.onCancel = undefined;
-                    signal.unsubscribe(sub);
-                    resolve(event);
-                }, filter);
+    constructor(signal: MinecraftAfterEventSignals, filter?: U) {
+        this.promise = new Promise<T | undefined>((resolve, _) => {
+            if (signal === undefined || signal.subscribe === undefined || signal.unsubscribe === undefined) {
+                resolve(undefined);
+                return;
             }
 
-            cancelFn = () => {
-                signal.unsubscribe(sub);
+            const sub = (event: T) => {
+                this.onCancel = undefined;
+                signal.unsubscribe(sub as never);
+                resolve(event);
+            };
+            if (filter === undefined) {
+                signal.subscribe(sub as never);
+            } else {
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                (signal.subscribe as (listener: (event: T) => void, filter: U) => (...a: any) => void)(sub, filter);
+            }
+
+            this.onCancel = () => {
+                signal.unsubscribe(sub as never);
                 resolve(undefined);
             };
         });
-        this.onCancel = cancelFn;
+    }
+
+    cancel() {
+        this.onCancel?.();
+    }
+
+    then<TFulfill = T | undefined, TReject = never>(
+        onfulfilled?: ((value: T | undefined) => TFulfill | PromiseLike<TFulfill>) | null,
+        onrejected?: ((reason: unknown) => TReject | PromiseLike<TReject>) | null
+    ): Promise<TFulfill | TReject> {
+        return this.promise.then(onfulfilled, onrejected);
     }
 
     /**
-     * Cancels the promise by resolving it with undefined and unsubscribing from the event signal.
+     * Promise-like interface catch.
+     *
+     * @param onrejected Called if the promise rejects
      */
-    cancel() {
-        this.onCancel?.();
+    catch<TReject = never>(
+        onrejected?: ((reason: unknown) => TReject | PromiseLike<TReject>) | null
+    ): Promise<T | undefined | TReject> {
+        return this.promise.catch(onrejected);
+    }
+
+    /**
+     * Promise-like interface finally.
+     *
+     * @param onfinally Called when the promise resolves
+     */
+    finally(onfinally?: (() => void) | null): Promise<T | undefined> {
+        return this.promise.finally(onfinally);
     }
 }

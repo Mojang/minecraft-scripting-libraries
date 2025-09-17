f21880f78722008a4370bf51664430e60e49ca85
Andrew Griffin <agriffin@microsoft.com>
2025-09-17 14:02:13 -0700

    Tests passing

--- libraries/gameplay-utilities/src/events/eventPromise.test.ts
+++ libraries/gameplay-utilities/src/events/eventPromise.test.ts
@@ -1,35 +1,65 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-import { describe, expect, it, vi } from 'vitest';
-import { nextEvent } from './eventPromise.js';
-import { WeatherChangeAfterEvent } from '@minecraft/server';
+import { afterEach, describe, it, vi, expect } from 'vitest';
 
-function createWorldMock() {
-    return {
-        afterEvents: {
-            weatherChange: {
-                subscribe: vi.fn(),
-                unsubscribe: vi.fn(),
-            },
-            entityDie: {
-                subscribe: vi.fn(),
-                unsubscribe: vi.fn(),
-            },
-        },
-    };
-}
+import {
+    clearMockState,
+    createMockServerBindings,
+    MockWeatherChangeEventHandlers,
+    createWeatherEvent,
+    MockEntityRemoveEventHandlers,
+} from '../../__mocks__/minecraft-server.js';
+
+vi.mock('@minecraft/server', () => createMockServerBindings());
+
+import { nextEvent } from './eventPromise.js';
 
 describe('EventPromise', () => {
+    afterEach(() => {
+        vi.restoreAllMocks();
+        clearMockState();
+    });
+
     it('Event is subscribed', () => {
-        const world = createWorldMock();
-        let receivedCallback: ((event: WeatherChangeAfterEvent) => void) | undefined = undefined;
-        expect(receivedCallback).toBeUndefined();
-        vi.spyOn(world.afterEvents.weatherChange, 'subscribe').mockImplementation(callback => {
-            receivedCallback = callback as (event: WeatherChangeAfterEvent) => void;
+        const server = createMockServerBindings();
+        void nextEvent(server.world.afterEvents.weatherChange);
+        expect(MockWeatherChangeEventHandlers.length).toBe(1);
+    });
+
+    // specifically this test differs from above because this signal supports filters
+    it('Event is subscribed without filter', () => {
+        const server = createMockServerBindings();
+        void nextEvent(server.world.afterEvents.entityRemove);
+        expect(MockEntityRemoveEventHandlers.length).toBe(1);
+    });
+
+    it('Event is subscribed with filter', () => {
+        const server = createMockServerBindings();
+        void nextEvent(server.world.afterEvents.entityRemove, { entityTypes: ['foobar'] });
+        expect(MockEntityRemoveEventHandlers.length).toBe(1);
+    });
+
+    it('Event is unsubscribed when called', async () => {
+        const server = createMockServerBindings();
+        const prom = nextEvent(server.world.afterEvents.weatherChange);
+        const event = createWeatherEvent('foo');
+        MockWeatherChangeEventHandlers.forEach(handler => {
+            handler(event);
         });
-        void nextEvent(world.afterEvents.weatherChange);
-        expect(world.afterEvents.weatherChange.subscribe).toBeCalled();
-        expect(receivedCallback).toBeDefined();
+        await prom;
+        expect(MockWeatherChangeEventHandlers.length).toBe(0);
+    });
+
+    it('Event is gathered from await promise', async () => {
+        const server = createMockServerBindings();
+        const eventExpected = createWeatherEvent('foobar');
+        setTimeout(() => {
+            MockWeatherChangeEventHandlers.forEach(handler => {
+                handler(eventExpected);
+            });
+        }, 100);
+        const eventActual = await nextEvent(server.world.afterEvents.weatherChange);
+        expect(eventActual).toBe(eventExpected);
     });
 });
--- libraries/gameplay-utilities/vite.config.mts
+++ libraries/gameplay-utilities/vite.config.mts
@@ -5,5 +5,11 @@
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    test: { exclude: [...configDefaults.exclude, '**/build/**', '**/lib/**', '**/lib-commonjs/**'], watch: false },
+    test: {
+        exclude: [...configDefaults.exclude, '**/build/**', '**/lib/**', '**/lib-commonjs/**'],
+        watch: false,
+        alias: {
+            '@minecraft/server': './__mocks__/minecraft-server.ts',
+        },
+    },
 });

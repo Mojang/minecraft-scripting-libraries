47e7ab2163c9617aaf83623e4e95911a447084ad
Niamh Cuileann (she/her) - Skybox <136374332+sweetniamhy@users.noreply.github.com>
2026-01-06 13:06:47 -0500

    AABBUtils and Vector3Utils.ceil (#99)
    * Added AABBUtils and Vector3Utils.ceil
    
    * Change files
    
    * Removing AABB temp interface
    
    * API report
    
    * Added error for invalid AABB
    
    * Cleanup error message to only specify extent
    
    * Adjusted block volume tests to be more clear of the expected block dimensions
    
    * Changed the zero extent check to use epsilon
    
    * Reworded EPSILON doc
    
    * Added throw comments, cleaned up docs
    
    * Update change/@minecraft-math-13da4e91-9583-4450-b4b9-a00da5a6a7c9.json
    
    * PR feedback, removed beachball to regen
    
    * Adding min/max to Vector3Utils and builder. Added tests for min/max/ceil core helpers and builder. Updated to use min/max in AABBUtils.
    
    * Change files

---
+++ libraries/math/src/aabb/coreHelpers.ts
@@ -0,0 +1,318 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import type { AABB, Vector3 } from '@minecraft/server';
+import { BlockVolume } from '@minecraft/server';
+import { Vector3Utils } from '../vector3/coreHelpers.js';
+
+/**
+ * An error that is thrown when using an invalid AABB with AABBUtils operations.
+ *
+ * @public
+ */
+export class AABBInvalidExtentError extends Error {
+    constructor(extent: Vector3) {
+        super(`Invalid AABB extent of '${Vector3Utils.toString(extent)}'`);
+    }
+}
+
+/**
+ * Utilities operating on AABB objects. All methods are static and do not modify the input objects.
+ *
+ * @public
+ */
+export class AABBUtils {
+    private constructor() {}
+
+    /**
+     * EPSILON
+     *
+     * The internal epsilon value that determines validity and used for block volume tolerance.
+     */
+    static EPSILON = 0.00001;
+
+    /**
+     * createFromCornerPoints
+     *
+     * Gets an AABB from points defining it's corners, the order doesn't matter.
+     * @param pointA - The first corner point.
+     * @param pointB - The second corner point.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the resulting AABB is invalid.
+     *
+     * @returns - The resulting AABB.
+     */
+    static createFromCornerPoints(pointA: Vector3, pointB: Vector3): AABB {
+        const min = Vector3Utils.min(pointA, pointB);
+        const max = Vector3Utils.max(pointA, pointB);
+
+        const extent = Vector3Utils.multiply(Vector3Utils.subtract(max, min), { x: 0.5, y: 0.5, z: 0.5 });
+        const aabb: AABB = { center: Vector3Utils.add(min, extent), extent: extent };
+        AABBUtils.throwErrorIfInvalid(aabb);
+        return aabb;
+    }
+
+    /**
+     * isValid
+     *
+     * Determines if the AABB has non-zero extent on all axes.
+     * @param aabb - The AABB to test for validity.
+     * @returns - True if all extent axes are non-zero, otherwise false.
+     */
+    static isValid(aabb: AABB): boolean {
+        return (
+            aabb.extent.x >= AABBUtils.EPSILON &&
+            aabb.extent.y >= AABBUtils.EPSILON &&
+            aabb.extent.z >= AABBUtils.EPSILON
+        );
+    }
+
+    /**
+     * throwErrorIfInvalid
+     *
+     * Throws an error if the AABB is invalid.
+     * @param aabb - The AABB to test for validity.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     */
+    static throwErrorIfInvalid(aabb: AABB): void {
+        if (!AABBUtils.isValid(aabb)) {
+            throw new AABBInvalidExtentError(aabb.extent);
+        }
+    }
+
+    /**
+     * equals
+     *
+     * Compares the equality of two AABBs.
+     * @param aabb - The first AABB in the comparison.
+     * @param other - The second AABB in the comparison.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if either of the input AABBs are invalid.
+     *
+     * @returns - True if the center and extent of both AABBs are equal.
+     */
+    static equals(aabb: AABB, other: AABB): boolean {
+        AABBUtils.throwErrorIfInvalid(aabb);
+        AABBUtils.throwErrorIfInvalid(other);
+
+        return Vector3Utils.equals(aabb.center, other.center) && Vector3Utils.equals(aabb.extent, other.extent);
+    }
+
+    /**
+     * getMin
+     *
+     * Gets the minimum corner of an AABB.
+     * @param aabb - The AABB to retrieve the minimum corner of.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     *
+     * @returns - The minimum corner of the AABB.
+     */
+    static getMin(aabb: AABB): Vector3 {
+        AABBUtils.throwErrorIfInvalid(aabb);
+
+        return Vector3Utils.subtract(aabb.center, aabb.extent);
+    }
+
+    /**
+     * getMax
+     *
+     * Gets the maximum corner of an AABB.
+     * @param aabb - The AABB to retrieve the maximum corner of.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     *
+     * @returns - The maximum corner of the AABB.
+     */
+    static getMax(aabb: AABB): Vector3 {
+        AABBUtils.throwErrorIfInvalid(aabb);
+
+        return Vector3Utils.add(aabb.center, aabb.extent);
+    }
+
+    /**
+     * getSpan
+     *
+     * Gets the span of an AABB.
+     * @param aabb - The AABB to retrieve the span of.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     *
+     * @returns - The span of the AABB.
+     */
+    static getSpan(aabb: AABB): Vector3 {
+        AABBUtils.throwErrorIfInvalid(aabb);
+
+        return Vector3Utils.multiply(aabb.extent, { x: 2.0, y: 2.0, z: 2.0 });
+    }
+
+    /**
+     * getBlockVolume
+     *
+     * Creates the smallest BlockVolume that includes all of a source AABB.
+     * @param aabb - The source AABB.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     *
+     * @returns - The BlockVolume containing the source AABB.
+     */
+    static getBlockVolume(aabb: AABB): BlockVolume {
+        AABBUtils.throwErrorIfInvalid(aabb);
+
+        const epsilon = AABBUtils.EPSILON;
+        const epsilonVec: Vector3 = { x: epsilon, y: epsilon, z: epsilon };
+        const from = Vector3Utils.floor(Vector3Utils.add(AABBUtils.getMin(aabb), epsilonVec));
+        const to = Vector3Utils.ceil(Vector3Utils.subtract(AABBUtils.getMax(aabb), epsilonVec));
+        return new BlockVolume(from, to);
+    }
+
+    /**
+     * translate
+     *
+     * Creates a translated AABB given a source AABB and translation vector.
+     * @param aabb - The source AABB.
+     * @param delta - The translation vector to add to the AABBs center.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     *
+     * @returns - The resulting translated AABB.
+     */
+    static translate(aabb: AABB, delta: Vector3): AABB {
+        AABBUtils.throwErrorIfInvalid(aabb);
+
+        return { center: Vector3Utils.add(aabb.center, delta), extent: aabb.extent };
+    }
+
+    /**
+     * dilate
+     *
+     * Creates a dilated AABB given a source AABB and dilation vector.
+     * @param aabb - The source AABB.
+     * @param size - The dilation vector to add to the AABBs extent.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     *
+     * @returns - The resulting dilated AABB.
+     */
+    static dilate(aabb: AABB, size: Vector3): AABB {
+        AABBUtils.throwErrorIfInvalid(aabb);
+
+        const epsilon = AABBUtils.EPSILON;
+        const epsilonVec: Vector3 = { x: epsilon, y: epsilon, z: epsilon };
+        let dilatedExtent = Vector3Utils.add(aabb.extent, size);
+        dilatedExtent = Vector3Utils.clamp(dilatedExtent, { min: epsilonVec });
+        return { center: aabb.center, extent: dilatedExtent };
+    }
+
+    /**
+     * expand
+     *
+     * Creates an expanded AABB given two source AABBs.
+     * @param aabb - The first source AABB.
+     * @param other - The second source AABB.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if either of the input AABBs are invalid.
+     *
+     * @returns - The resulting expanded AABB.
+     */
+    static expand(aabb: AABB, other: AABB): AABB {
+        AABBUtils.throwErrorIfInvalid(aabb);
+        AABBUtils.throwErrorIfInvalid(other);
+
+        const aabbMin = AABBUtils.getMin(aabb);
+        const otherMin = AABBUtils.getMin(other);
+        const min = Vector3Utils.min(aabbMin, otherMin);
+        const aabbMax = AABBUtils.getMax(aabb);
+        const otherMax = AABBUtils.getMax(other);
+        const max = Vector3Utils.max(aabbMax, otherMax);
+        return AABBUtils.createFromCornerPoints(min, max);
+    }
+
+    /**
+     * getIntersection
+     *
+     * Creates an AABB of the intersecting area of two source AABBs.
+     * @param aabb - The first source AABB.
+     * @param other - The second source AABB.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if either of the input AABBs are invalid.
+     *
+     * @returns - The resulting intersecting AABB if they intersect, otherwise returns undefined.
+     */
+    static getIntersection(aabb: AABB, other: AABB): AABB | undefined {
+        AABBUtils.throwErrorIfInvalid(aabb);
+        AABBUtils.throwErrorIfInvalid(other);
+
+        if (!AABBUtils.intersects(aabb, other)) {
+            return undefined;
+        }
+
+        const aabbMin = AABBUtils.getMin(aabb);
+        const otherMin = AABBUtils.getMin(other);
+        const min = Vector3Utils.max(aabbMin, otherMin);
+        const aabbMax = AABBUtils.getMax(aabb);
+        const otherMax = AABBUtils.getMax(other);
+        const max = Vector3Utils.min(aabbMax, otherMax);
+        return AABBUtils.createFromCornerPoints(min, max);
+    }
+
+    /**
+     * intersects
+     *
+     * Calculates if two AABBs are intersecting.
+     * @param aabb - The first AABB.
+     * @param other - The second AABB.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if either of the input AABBs are invalid.
+     *
+     * @returns - True if the AABBs are intersecting, otherwise false.
+     */
+    static intersects(aabb: AABB, other: AABB): boolean {
+        AABBUtils.throwErrorIfInvalid(aabb);
+        AABBUtils.throwErrorIfInvalid(other);
+
+        const aabbMin = AABBUtils.getMin(aabb);
+        const aabbMax = AABBUtils.getMax(aabb);
+        const otherMin = AABBUtils.getMin(other);
+        const otherMax = AABBUtils.getMax(other);
+
+        if (otherMax.x < aabbMin.x || otherMin.x > aabbMax.x) {
+            return false;
+        }
+        if (otherMax.y < aabbMin.y || otherMin.y > aabbMax.y) {
+            return false;
+        }
+        if (otherMax.z < aabbMin.z || otherMin.z > aabbMax.z) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * isInside
+     *
+     * Calculates if a position is inside of an AABB.
+     * @param aabb - The AABB to test against.
+     * @param pos - The position to test.
+     * @throws {@link AABBInvalidExtentError}
+     * This exception is thrown if the input AABB is invalid.
+     *
+     * @returns True if the position is inside of the AABB, otherwise returns false.
+     */
+    static isInside(aabb: AABB, pos: Vector3): boolean {
+        AABBUtils.throwErrorIfInvalid(aabb);
+
+        const min = AABBUtils.getMin(aabb);
+        if (pos.x < min.x || pos.y < min.y || pos.z < min.z) {
+            return false;
+        }
+
+        const max = AABBUtils.getMax(aabb);
+        if (pos.x > max.x || pos.y > max.y || pos.z > max.z) {
+            return false;
+        }
+
+        return true;
+    }
+}

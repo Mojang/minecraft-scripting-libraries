9ff6388df28154c7dd21815c34f40a5238d0c311
alexdenfordskyboxlabs <103449886+AlexDenfordSkyboxLabs@users.noreply.github.com>
2026-02-24 13:04:35 -0800

    Adding color constants (#119)

---
+++ libraries/math/src/general/colorUtils.ts
@@ -0,0 +1,32 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+/**
+ * A set of commonly used color constant values.
+ *
+ * @public
+ */
+export const Colors = {
+    Black: { red: 29 / 255, green: 29 / 255, blue: 33 / 255, alpha: 1 },
+    Blue: { red: 60 / 255, green: 68 / 255, blue: 170 / 255, alpha: 1 },
+    Brown: { red: 131 / 255, green: 84 / 255, blue: 50 / 255, alpha: 1 },
+    Cyan: { red: 22 / 255, green: 156 / 255, blue: 156 / 255, alpha: 1 },
+    Gray: { red: 71 / 255, green: 79 / 255, blue: 82 / 255, alpha: 1 },
+    Green: { red: 94 / 255, green: 124 / 255, blue: 22 / 255, alpha: 1 },
+    LightBlue: { red: 58 / 255, green: 179 / 255, blue: 218 / 255, alpha: 1 },
+    Lime: { red: 128 / 255, green: 199 / 255, blue: 31 / 255, alpha: 1 },
+    Magenta: { red: 199 / 255, green: 78 / 255, blue: 189 / 255, alpha: 1 },
+    Orange: { red: 249 / 255, green: 128 / 255, blue: 29 / 255, alpha: 1 },
+    Pink: { red: 243 / 255, green: 139 / 255, blue: 170 / 255, alpha: 1 },
+    Purple: { red: 137 / 255, green: 50 / 255, blue: 184 / 255, alpha: 1 },
+    Red: { red: 176 / 255, green: 46 / 255, blue: 38 / 255, alpha: 1 },
+    Silver: { red: 157 / 255, green: 157 / 255, blue: 151 / 255, alpha: 1 },
+    White: { red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1 },
+    Yellow: { red: 254 / 255, green: 216 / 255, blue: 61 / 255, alpha: 1 },
+    PureWhite: { red: 1, green: 1, blue: 1, alpha: 1 },
+    PureBlack: { red: 0, green: 0, blue: 0, alpha: 1 },
+    PureRed: { red: 1, green: 0, blue: 0, alpha: 1 },
+    PureGreen: { red: 0, green: 1, blue: 0, alpha: 1 },
+    PureBlue: { red: 0, green: 0, blue: 1, alpha: 1 },
+    Transparent: { red: 0, green: 0, blue: 0, alpha: 0 },
+};

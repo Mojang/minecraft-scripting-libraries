808a488d4435c8ddcf3baff580b9b0eadf3e7e29
Brandon Chan <brandon.chan@skyboxlabs.com>
2026-08-14 16:51:37 -0700

    Adjust env variables

--- .github/scripts/notify-on-error.sh
+++ .github/scripts/notify-on-error.sh
@@ -2,6 +2,6 @@
 if [ -z "$test_secret" ]; then
     echo "Could not find secret"
 else
-    short_sha="${GITHUB_SHA::6}"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"*Open Source Pipeline Failure!*\n>An issue occurred in commit: <${{github.server_url}}/${{github.repository}}/commit/${{github.sha}}|${short_sha}...>\"}" $test_secret
+    short_sha="${sha:0:6}"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"*Open Source Pipeline Failure!*\n>An issue occurred in commit: <${server_url}/${repository}/commit/${sha}|${short_sha}...>\"}" "$test_secret"
 fi

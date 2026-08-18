fad97592460f024e1f075280ada2079cc26dd8ac
Brandon Chan <brandon.chan@skyboxlabs.com>
2026-08-18 15:37:50 -0700

    Rename test_secret to webhook_secret

--- .github/scripts/notify-on-error.sh
+++ .github/scripts/notify-on-error.sh
@@ -1,7 +1,7 @@
 
-if [ -z "$test_secret" ]; then
+if [ -z "$webhook_secret" ]; then
     echo "Could not find secret"
 else
     short_sha="${sha:0:6}"
-    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"*Open Source Pipeline Failure!*\n>An issue occurred in commit: <${server_url}/${repository}/commit/${sha}|${short_sha}...>\"}" "$test_secret"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"*Open Source Pipeline Failure!*\n>An issue occurred in commit: <${server_url}/${repository}/commit/${sha}|${short_sha}...>\"}" "$webhook_secret"
 fi
